991f25db158841c1d93274b2e4e56ab80c15446c
ElizabethCherrie <leobetta77@gmail.com>
2025-08-13 21:42:23 +0100

    Task2

---
+++ Arrays&Hashing/ValidAnagram.py
@@ -0,0 +1,12 @@
+""" Task2 Valid Anagram
+Given two strings s and t, return true if the two strings are 
+anagrams of each other, otherwise return false.
+An anagram is a string that contains the exact same characters 
+as another string, but the order of the characters can be different."""
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        count = 0
+        if len(s) != len(t):
+            return False
+        return sorted(s) == sorted(t)
