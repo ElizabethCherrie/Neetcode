70de37a56517f3b793b091dc73135dd3fe5a0bdd
ElizabethCherrie <leobetta77@gmail.com>
2025-08-20 01:44:08 +0100

    Task2

---
+++ LinkedList/2_MergeTwoSortedLinkedLists.py
@@ -0,0 +1,27 @@
+"""Task 2 Merge Two Sorted Linked Lists
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+The new list should be made up of nodes from list1 and list2."""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        zero_pos = merged = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                merged.next = list1
+                list1 = list1.next
+            else:
+                merged.next = list2
+                list2 = list2.next
+            merged = merged.next
+        if list1:
+            merged.next = list1
+        elif list2:
+            merged.next = list2
+        return zero_pos.next
