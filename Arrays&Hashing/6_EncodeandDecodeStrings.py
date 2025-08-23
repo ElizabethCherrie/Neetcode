2f578d3a1eafbdceffcd7210a26580af6a45d75b
ElizabethCherrie <leobetta77@gmail.com>
2025-08-23 19:44:43 +0100

    Task6

---
+++ Arrays&Hashing/6_EncodeandDecodeStrings.py
@@ -0,0 +1,29 @@
+"""Task 6 Encode and Decode Strings
+Design an algorithm to encode a list of strings to a single string. 
+The encoded string is then decoded back to the original list of strings.
+Please implement encode and decode
+"""
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for i in strs:
+            res += str(len(i)) + "@" + i
+        return res
+        
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "@":
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = i + length
+            res.append(s[i:j])
+            i = j
+
+        return res
