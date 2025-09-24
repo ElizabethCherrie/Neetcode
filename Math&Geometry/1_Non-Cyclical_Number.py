4102d4cdab69071fe0b4b9c23be66a138f0965f7
ElizabethCherrie <leobetta77@gmail.com>
2025-09-24 20:17:50 +0100

    Task1

---
+++ Math&Geometry/1_Non-Cyclical_Number.py
@@ -0,0 +1,29 @@
+"""Task 1 Non-Cyclical Number
+A non-cyclical number is an integer defined by the following algorithm:
+
+Given a positive integer, replace it with the sum of the squares of its digits.
+Repeat the above step until the number equals 1, or it loops infinitely in a cycle which does not include 1.
+If it stops at 1, then the number is a non-cyclical number.
+Given a positive integer n, return true if it is a non-cyclical number, otherwise return false.
+"""
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visit = set()
+
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfSquares(n)
+            if n == 1:
+                return True
+        return False
+
+    def sumOfSquares(self, n: int) -> int:
+            output = 0
+
+            while n:
+                digit = n % 10
+                digit = digit ** 2
+                output += digit
+                n = n // 10
+            return output
