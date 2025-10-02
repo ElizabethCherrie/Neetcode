3158b57badedaae46aed3d20a21d2ba0b3b4d5f3
ElizabethCherrie <leobetta77@gmail.com>
2025-10-02 19:48:21 +0100

    Task6

---
+++ BitManipulation/6_Sum_of_Two_Integers.py
@@ -0,0 +1,14 @@
+"""Task 6 Sum of Two Integers
+Given two integers a and b, return the sum of the two integers without using the + and - operators."""
+
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        max_int = 0x7FFFFFFF
+
+        while b != 0:
+            carry = (a & b) << 1
+            a = (a ^ b) & mask
+            b = carry & mask
+
+        return a if a <= max_int else ~(a ^ mask)
