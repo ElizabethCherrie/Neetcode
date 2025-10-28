e52a5af462f99c77e610bae2ffa55c83189bbafc
ElizabethCherrie <leobetta77@gmail.com>
2025-10-28 14:54:07 +0000

    Task11

---
+++ DynamicPro1/11_Longest_Increasing_Subsequence.py
@@ -0,0 +1,19 @@
+"""Task 11 Longest Increasing Subsequence
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+A subsequence is a sequence that can be derived from the given sequence by deleting some or no elements 
+without changing the relative order of the remaining characters.
+
+For example, "cat" is a subsequence of "crabt".
+"""
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        LIS = [1] * len(nums)
+        
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+                    
+        return max(LIS)
