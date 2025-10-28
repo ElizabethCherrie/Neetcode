87a0937085d526dbed846ee35e5b53ab254cb93b
ElizabethCherrie <leobetta77@gmail.com>
2025-10-28 09:35:36 +0000

    Task1

---
+++ Intervals/1_Meeting_Rooms.py
@@ -0,0 +1,25 @@
+"""Task 1 Meeting Rooms
+
+Given an array of meeting time interval objects consisting of start and end times [[start_1,end_1],[start_2,end_2],...] 
+(start_i < end_i), determine if a person could add all meetings to their schedule without any conflicts.
+
+"""
+
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+
+        intervals.sort(key = lambda x: x.start)
+        
+        for i in range(len(intervals) - 1):
+            if intervals[i].end > intervals[i+1].start:
+                return False
+            
+        return True
