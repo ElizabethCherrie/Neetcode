588e6c9d35e72486e63f76f06d7ef16c23b4533b
ElizabethCherrie <leobetta77@gmail.com>
2025-11-09 22:06:11 +0000

    Task2169

---
+++ Simulations/2169_Count_Operations_to_Obtain_Zero.py
@@ -0,0 +1,22 @@
+"""Task 2169. Count Operations to Obtain Zero
+
+You are given two non-negative integers num1 and num2.
+
+In one operation, if num1 >= num2, you must subtract num2 from num1, otherwise subtract num1 from num2.
+
+For example, if num1 = 5 and num2 = 4, subtract num2 from num1, thus obtaining num1 = 1 and num2 = 4. 
+However, if num1 = 4 and num2 = 5, after one operation, num1 = 4 and num2 = 1.
+Return the number of operations required to make either num1 = 0 or num2 = 0.
+
+"""
+
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        i=0
+        while num1 != 0 and num2 != 0:
+            if num1 >= num2:
+                num1 -= num2
+            else:
+                num2 -= num1
+            i+=1     
+        return i
