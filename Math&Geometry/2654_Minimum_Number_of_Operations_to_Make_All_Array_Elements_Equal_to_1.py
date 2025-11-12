e02a27d20b0435b18d7b879fd192d04a2b8bf6f9
ElizabethCherrie <leobetta77@gmail.com>
2025-11-12 23:29:12 +0000

    Task2654

---
+++ Math&Geometry/2654_Minimum_Number_of_Operations_to_Make_All_Array_Elements_Equal_to_1.py
@@ -0,0 +1,26 @@
+"""Task 2654. Minimum Number of Operations to Make All Array Elements Equal to 1
+
+You are given a 0-indexed array nums consisiting of positive integers. You can do the following operation on 
+the array any number of times:
+
+Select an index i such that 0 <= i < n - 1 and replace either of nums[i] or nums[i+1] with their gcd value.
+Return the minimum number of operations to make all elements of nums equal to 1. If it is impossible, return -1.
+
+The gcd of two integers is the greatest common divisor of the two integers.
+
+"""
+
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        n = len(nums)
+        ones = nums.count(1)
+        if ones: return n - ones
+        res = inf
+        for i in range(n):
+            g = nums[i]
+            for j in range(i + 1, n):
+                g = gcd(g, nums[j])
+                if g == 1:
+                    res = min(res, j - i)
+        if res == inf: return -1
+        return res + n - 1
