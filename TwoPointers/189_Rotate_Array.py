4dee35d5569bb8c75dadeba6952e5440a4599fbe
ElizabethCherrie <leobetta77@gmail.com>
2026-01-04 20:21:07 +0000

    Task189

---
+++ TwoPointers/189_Rotate_Array.py
@@ -0,0 +1,19 @@
+"""Task 189. Rotate Array
+
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+"""
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k %= len(nums)
+        def rev(l, r):
+            while l < r:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+                r -= 1
+        
+        rev(0, len(nums) - 1)
+        rev(0, k - 1)
+        rev(k, len(nums) - 1)
